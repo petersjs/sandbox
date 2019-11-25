4b1b99736df608011cef41918971f143f355b508
Joe Peterson <petersjs87@gmail.com>
2019-11-25 15:26:31 -0600

    Testing out new project commit

---
+++ src/Hello.java
@@ -0,0 +1,24 @@
+/** */
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("hello");
+    System.out.println("hello");
+    System.out.println("hello");
+
+    System.out.println("hello");
+
+    int x = 0;
+    if (true) {
+      x++;
+      if (false) {
+        x--;
+      }
+    }
+  }
+
+
+
+  public static int foo(int x, int y) {
+      return x*y;
+  }
+}
---
+++ src/com/jp/sandbox/Hello.java
@@ -0,0 +1,7 @@
+package com.jp.sandbox;
+
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("This is hello 2!");
+    }
+}
